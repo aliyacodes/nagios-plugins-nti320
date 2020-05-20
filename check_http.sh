63afdddd49b16b268b59c2a4d754b99756169bf9
Eli <vincent.brown@seattlecolleges.edu>
2020-05-20 12:52:20 -0700

    Update and rename check_http to check_http.sh

--- check_http.sh
+++ check_http.sh
@@ -3,3 +3,8 @@
 # https://raw.githubusercontent.com/Eli-Brown/NTI-320-Atom-Smasher/master/nrpe-http-eli/check_http
 # https://github.com/Eli-Brown/NTI-320-Atom-Smasher/blob/master/nrpe-http-eli/check_http
 # nrpe-http-eli/check_http
+
+define command{
+    command_name  check_http.1
+    command_line  /usr/lib64/nagios/plugins/check_http.1  --url='$ARG1$' --extra_args='$ARG2$'
+}
