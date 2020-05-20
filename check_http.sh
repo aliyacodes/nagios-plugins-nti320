257076fb0436b96698e87267d40bc89589477564
Eli <vincent.brown@seattlecolleges.edu>
2020-05-20 13:28:47 -0700

    Update check_http.sh

--- check_http.sh
+++ check_http.sh
@@ -25,12 +25,7 @@ KEY=$ARG2
 CRIT=$3
 http_proxy=""
 
-DEBUG=
 
-if [ -z $CRIT ]; then
-echo "Usage $0 "
-exit 3
-fi
 
 TC=`echo ${URL} | awk -F. '{print $1}' |awk -F/ '{print $NF}'`
 TMP="/tmp/check_http_sh_${TC}.tmp"
@@ -39,36 +34,21 @@ CMD_TIME="curl -k --location --no-buffer --silent --output ${TMP} -w %{time_conn
 TIME=`eval $CMD_TIME`
 
 if [ -f $TMP ]; then
-RESULT=`grep -c $KEY $TMP`
+RESULT=`grep -c $URL
 else
 echo "UNKOWN - Could not create tmp file $TMP"
 # exit 3
-fi
-
-TIMETOT=`echo $TIME | gawk -F: '{ print $3 }'`
-
-if [ ! -z $DEBUG ]; then
-echo "CMD_TIME: $CMD_TIME"
-echo "NUMBER OF $KEY FOUNDS: $RESULT"
-echo "TIMES: $TIME"
-echo "TIME TOTAL: $TIMETOT"
-echo "TMP: $TMP"
-ls $TMP
 fi
 
 rm -f $TMP
 
 SURL=`echo $URL | cut -d "/" -f3-4`
 
-MSGOK="Site $SURL key $KEY time $TIMETOT |'time'=${TIMETOT}s;${CRIT}"
-MSGKO="Site $SURL has problems, time $TIMETOT |'time'=${TIMETOT}s;${CRIT}"
 
-#PERFDATA HOWTO 'label'=value[UOM];[warn];[crit];[min];[max]
 
-if [ "$RESULT" -ge "1" ] && [ $(echo "$TIMETOT < $CRIT"|bc) -eq 1 ]; then
-echo "OK - $MSGOK"
+echo "OK - $ARG1"
 exit 0
 else
-echo "CRITICAL - $MSGKO"
+echo "HTTP STATUS & ADDRESS - $ARG1"
 exit 2
 fi
