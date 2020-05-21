719d57d3ac45ecacd16c478128061ec1986e705d
Eli <vincent.brown@seattlecolleges.edu>
2020-05-21 11:02:00 -0700

    Update check_http.sh

--- check_http.sh
+++ check_http.sh
@@ -7,14 +7,14 @@
 # nrpe-http-eli/check_http
 
 define command{
-    command_name  check_http.1
-    command_line  /usr/lib64/nagios/plugins/check_http.1  --url='$ARG1$' --extra_args='$ARG2$'
+    command_name  check_http.sh
+    command_line  /usr/lib64/nagios/plugins/check_http.sh  --url='$ARG1$' --extra_args='$ARG2$'
 }
 
 define service {
         host_name                       example
         service_description             service_name
-        check_command                   check_http.1!http://10.128.0.3/path!--proxy http://user:name@host:8080 --user user:name --ntlm
+        check_command                   check_http.sh!http://35.224.66.98/nagios/!--proxy http://user:name@host:8080 --user user:name --ntlm
         use                             generic-service
         notes                           some useful notes >>>not sure this is right
 }
@@ -23,18 +23,19 @@ define service {
 URL=$ARG1
 KEY=$ARG2
 
-curl -I -s -L <http://10.128.0.3> | grep "HTTP/1.1"
+curl -I -s -L <http://35.224.66.98/nagios/> | grep "HTTP/1.1"
 
 ######TEST1#####|#!/bin/bash
-curl -v http://34.66.99.124/nagios
+curl -v http://35.224.66.98/nagios/
 
-curl -s -o HTTP-Status-Response.txt -w "%{http_code}" http://34.66.99.124/nagios
+curl -s -o HTTP-Status-Response.txt -w "%{http_code}" http://35.224.66.98/nagios/
 
-http_HTTP-Status-Response=$(curl -s -o HTTP-Status-Response.txt -w "%{http_code}" http://34.66.99.124/nagios
+http_HTTP-Status-Response=$(curl -s -o HTTP-Status-Response.txt -w "%{http_code}" http://35.224.66.98/nagios/
 if [ $http_HTTP-Status-Response != "200" ]; then
 	echo "No HTTP Service error"
     # handle error
 else
     echo "Server returned:"
     cat HTTP-Status-Response.txt    
+    echo $ARG1
 fi
