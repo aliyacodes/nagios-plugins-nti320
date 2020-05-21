7f485357057b9c306bcffbcf965212b8fcdc7bfc
Eli <vincent.brown@seattlecolleges.edu>
2020-05-21 12:30:13 -0700

    Update check_http.sh

--- check_http.sh
+++ check_http.sh
@@ -1,37 +1,17 @@
-
-######TEST2#####|
 #!/bin/bash
+######TEST2#####|
 # on Nagios server 10.128.0.3
 # https://raw.githubusercontent.com/Eli-Brown/NTI-320-Atom-Smasher/master/nrpe-http-eli/check_http
 # https://github.com/Eli-Brown/NTI-320-Atom-Smasher/blob/master/nrpe-http-eli/check_http
 # nrpe-http-eli/check_http
 
-define command{
-    command_name  check_http.sh
-    command_line  /usr/lib64/nagios/plugins/check_http.sh  --url='$ARG1$' --extra_args='$ARG2$'
-}
+######TEST1#####
+curl -v http://35.224.66.98/nagios
 
-define service {
-        host_name                       example
-        service_description             service_name
-        check_command                   check_http.sh!http://35.224.66.98/nagios/!--proxy http://user:name@host:8080 --user user:name --ntlm
-        use                             generic-service
-        notes                           some useful notes >>>not sure this is right
-}
+curl -s -o http_HTTP_Status_Response.txt -w "%{http_code}" http://35.224.66.98/nagios
 
-
-URL=$ARG1
-KEY=$ARG2
-
-curl -I -s -L <http://35.224.66.98/nagios/> | grep "HTTP/1.1"
-
-######TEST1#####|#!/bin/bash
-curl -v http://35.224.66.98/nagios/
-
-curl -s -o HTTP-Status-Response.txt -w "%{http_code}" http://35.224.66.98/nagios/
-
-http_HTTP-Status-Response=$(curl -s -o HTTP-Status-Response.txt -w "%{http_code}" http://35.224.66.98/nagios/
-if [ $http_HTTP-Status-Response != "200" ]; then
+http_HTTP-Status-Response=$(curl -s -o http_HTTP_Status_Response.txt -w "%{http_code}" http://35.224.66.98/nagios/
+if [  $ http_HTTP_Status_Response != "200" ]; then
 	echo "No HTTP Service error"
     # handle error
 else
