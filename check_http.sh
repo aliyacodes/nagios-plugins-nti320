0e9bf45fd3831812b007eb6e0ccef20aafcb89f8
Eli <vincent.brown@seattlecolleges.edu>
2020-05-21 10:25:09 -0700

    Update check_http.sh

--- check_http.sh
+++ check_http.sh
@@ -1,4 +1,5 @@
 
+######TEST2#####|
 #!/bin/bash
 # on Nagios server 10.128.0.3
 # https://raw.githubusercontent.com/Eli-Brown/NTI-320-Atom-Smasher/master/nrpe-http-eli/check_http
@@ -23,4 +24,18 @@ URL=$ARG1
 KEY=$ARG2
 
 curl -I -s -L <http://10.128.0.3> | grep "HTTP/1.1"
+
+######TEST1#####|#!/bin/bash
+
+curl -v http://34.66.99.124/nagios
+
+curl -s -o HTTP-Status-Response.txt -w "%{http_code}" http://34.66.99.124/nagios
+
+http_HTTP-Status-Response=$(curl -s -o HTTP-Status-Response.txt -w "%{http_code}" http://34.66.99.124/nagios
+if [ $http_HTTP-Status-Response != "200" ]; then
+    # handle error
+else
+    echo "Server returned:"
+    cat HTTP-Status-Response.txt    
+fi
 
